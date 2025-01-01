839ec302e88167f710903709eb6db4e109299d0e
koheihida <koheihida04@gmail.com>
2025-01-01 22:40:27 +0900

    make @GetMapping

--- src/main/java/com/example/its/IndexController.java
+++ src/main/java/com/example/its/IndexController.java
@@ -1,10 +1,13 @@
 package com.example.its;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
+
+    @GetMapping
     @ResponseBody
     public String index() {
         return "<h1>Hello World</h1>";
