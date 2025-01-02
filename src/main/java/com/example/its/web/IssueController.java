ff6f0defbc733421e76234dd0a7722d530ec0d5a
koheihida <koheihida04@gmail.com>
2025-01-02 18:55:16 +0900

    edit IssueController

--- src/main/java/com/example/its/web/IssueController.java
+++ src/main/java/com/example/its/web/IssueController.java
@@ -1,4 +1,22 @@
 package com.example.its.web;
 
+import com.example.its.domain.issue.IssueEntity;
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
 public class IssueController {
+    @GetMapping("/issues")
+    public String showList(Model model) {
+        var issueList = List.of(
+            new IssueEntity(1L, "概要1", "説明1"),
+            new IssueEntity(2L, "概要2", "説明2"),
+            new IssueEntity(3L, "概要3", "説明3")
+        );
+        model.addAttribute("issueList", issueList);
+        return "issues/list";
+    }
 }
