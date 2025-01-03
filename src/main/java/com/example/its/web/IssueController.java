d8676b0d85244db4fb80874d4217848152568867
koheihida <koheihida04@gmail.com>
2025-01-04 00:09:33 +0900

    IssueService関連の実装

--- src/main/java/com/example/its/web/IssueController.java
+++ src/main/java/com/example/its/web/IssueController.java
@@ -1,6 +1,7 @@
 package com.example.its.web;
 
 import com.example.its.domain.issue.IssueEntity;
+import com.example.its.domain.issue.IssueService;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,14 +10,12 @@ import java.util.List;
 
 @Controller
 public class IssueController {
+
+    private final IssueService issueService = new IssueService();
+
     @GetMapping("/issues")
     public String showList(Model model) {
-        var issueList = List.of(
-            new IssueEntity(1L, "概要1", "説明1"),
-            new IssueEntity(2L, "概要2", "説明2"),
-            new IssueEntity(3L, "概要3", "説明3")
-        );
-        model.addAttribute("issueList", issueList);
+        model.addAttribute("issueList", issueService.findAll());
         return "issues/list";
     }
 }
