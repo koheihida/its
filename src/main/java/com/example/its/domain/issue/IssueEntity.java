9f949f87624f883cb933776a0a99514df5c89c6b
koheihida <koheihida04@gmail.com>
2025-01-03 23:40:10 +0900

    @Dataの導入

--- src/main/java/com/example/its/domain/issue/IssueEntity.java
+++ src/main/java/com/example/its/domain/issue/IssueEntity.java
@@ -1,34 +1,12 @@
 package com.example.its.domain.issue;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
 @AllArgsConstructor
+@Data
 public class IssueEntity {
     private Long   id;
     private String summary;
     private String description;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getSummary() {
-        return summary;
-    }
-
-    public void setSummary(String summary) {
-        this.summary = summary;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
